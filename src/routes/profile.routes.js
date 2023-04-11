db56fa01152b308a3054a51f5de3de3331ee5304
Mayen624 <juliomayen300@gmail.com>
2023-04-11 16:34:12 -0300

    commit for save

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -2,8 +2,15 @@ const {Router} = require('express');
 const main = require('../controllers/mainController');
 const profileController = main.mainController.profileControler;
 const {isAuthenticated} = require('../helpers/sessionAuth');
+const { previousFriday } = require('date-fns');
 const router = Router();
 
 router.get('/', isAuthenticated,profileController.profileRender);
+
+router.post('/edit', isAuthenticated, profileController.editProfile);
+
+router.post('/secretKey', isAuthenticated, profileController.addSecretKey);
+
+router.get('/forgotKey', isAuthenticated, profileController.forgotSecretKey);
 
 module.exports = router;
